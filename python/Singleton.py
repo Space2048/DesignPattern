270766332f9b948fa937e9101caf9076cb0d0185
BAI001 <48885077+Space2048@users.noreply.github.com>
2024-11-25 15:49:24 +0800

    单例模式

---
+++ python/Singleton.py
@@ -0,0 +1,69 @@
+# 单例模型
+# 方式1：使用python 模块在模块中初始化对象并引入
+
+# 方式2：使用装饰器
+
+def Singleton(cls):
+    _instance = {}
+    
+    def _singleton(*args, **kargs):
+        if cls not in _instance:
+            _instance[cls] = cls(*args, **kargs)
+        return _instance[cls]
+    return _singleton
+
+@Singleton
+class A(object):
+    a = 1
+    def __init__(self, x=0):
+        self.x = x
+        
+        
+# 方式3：使用类
+import time
+import threading
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        time.sleep(1)
+
+    @classmethod
+    def instance(cls, *args, **kwargs):
+        with Singleton._instance_lock:
+            if not hasattr(Singleton, "_instance"):
+                Singleton._instance = Singleton(*args, **kwargs)
+        return Singleton._instance
+    
+    
+# 方式4：使用__new__
+import threading
+class Singleton(object):
+    _instance_lock = threading.Lock()
+
+    def __init__(self):
+        pass
+
+
+    def __new__(cls, *args, **kwargs):
+        if not hasattr(Singleton, "_instance"):
+            with Singleton._instance_lock:
+                if not hasattr(Singleton, "_instance"):
+                    Singleton._instance = object.__new__(cls)  
+        return Singleton._instance
+    
+# 方式5：：使用元类
+import threading
+
+class SingletonType(type):
+    _instance_lock = threading.Lock()
+    def __call__(cls, *args, **kwargs):
+        if not hasattr(cls, "_instance"):
+            with SingletonType._instance_lock:
+                if not hasattr(cls, "_instance"):
+                    cls._instance = super(SingletonType,cls).__call__(*args, **kwargs)
+        return cls._instance
+
+class Foo(metaclass=SingletonType):
+    def __init__(self,name):
+        self.name = name
